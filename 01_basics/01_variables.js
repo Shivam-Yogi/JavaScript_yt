e1561e719ce51cf010de804fb1cb3ed31297970e
Shivam_yogi <shivamyogi99@gmail.com>
2024-01-24 13:21:08 +0530

    second lecture done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+console.log("Shivam")
+
+const accountId = 144553 // Its to remain same throughout the code
+var accountEmail = "Shivam@gmail.com" // cant handle scopes properly
+let accountPassword = "123456" // better to do
+accountCity = "Delhi" // bad practise
+
+// accountId = 2 not allowed
+
+/*
+Prefer not to user 
+var because the block scope issues
+*/
+accountEmail = "hack@gmail.com"
+accountPassword = "789456"
+accountCity = "Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all js code as newer versions
+
+// alert(3+3) // for browser or in html not for nodejs
+
+console.log(3 + 2)
+
+console.log("Shivam")
+
+// code readability shoud be high
+// It should contain DRY rules
+
+// ECMA -> Javascript standards
+
+let name = "Shivam"
+let age = 21
+let isLoggedIn = false
+let state = null
+let hobby;
+
+
+
+// number => 2 to power of 53
+// bigInt => bigger numbers 
+// String => double quotes
+// boolean => true or false
+// null => used when nothing is there
+// undefined => variable which is declared but not initialized
+// object => collection of properties
+// array => ordered and unordered collection of items
+// type coercion => automatic conversion of data types
+// symbol => unique
+
+console.log(typeof "shivam")
+console.log(typeof(age))
+console.log(typeof(state)) // object
+console.log(typeof(undefined)) // undefined
+
+
+
