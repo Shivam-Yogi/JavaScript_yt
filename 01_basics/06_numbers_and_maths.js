7223a9b22ebb4a4323136099dcaa2488bdd248fa
Shivam_yogi <shivamyogi99@gmail.com>
2024-01-25 13:38:49 +0530

    math and num added

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,55 @@
+const score = 400
+console.log(score)
+
+
+const balance = new Number(100)
+console.log(balance)
+
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString())
+
+console.log(hundreds.toLocaleString('en-IN'))
+
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
+
+// ********************************************************************Maths*****************************************************************************************
+console.log(Math)
+
+console.log(Math.abs(-4))
+
+console.log(Math.ceil(12.35))
+
+console.log(Math.round(12.45))
+
+console.log(Math.floor(11.99))
+
+console.log(Math.min(11, 99, 3, 44, 55))
+
+console.log(Math.max(11, 99, 3, 44, 55))
+
+console.log(Math.random())
+
+console.log((Math.random()*10) + 1)
+
+const min = 10
+
+const max = 100
+
+console.log(Math.round(Math.random() * (max - min + 1) + min))
+
+
+
+
+
+
