642fcd2b6c24537006302c11604468d5cd3fcef5
Shivam_yogi <shivamyogi99@gmail.com>
2024-01-27 15:07:49 +0530

    added basics of array file

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,70 @@
+// Arrays
+
+const myArr = [0, 1, 2, 3, 4]
+
+const myHeroes = ["shaktiman", "naagraj"]
+
+// console.log(myArr[0]);
+
+// // Arrays methods
+
+// myArr.push(6)
+
+// console.log(myArr)
+
+// myArr.pop()
+
+// console.log(myArr)
+
+// myArr.unshift(0) // time consuming
+
+// console.log(myArr)
+
+// myArr.shift()
+
+// console.log(myArr)
+
+// console.log(myArr.includes(5));
+
+// console.log(myArr.indexOf(9))
+
+// console.log(myArr.indexOf(4))
+
+
+// const newArr = myArr.join()
+
+// console.log(newArr)
+// console.log(typeof newArr)
+
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myArr1 = myArr.slice(1,3);
+
+console.log(myArr1);
+
+console.log("B", myArr);
+
+
+
+const myArr2 = myArr.splice(1, 3)
+
+console.log(myArr2)
+
+console.log("C", myArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
