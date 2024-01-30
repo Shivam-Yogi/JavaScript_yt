cd804201582bebed4ad8ef16400e427d8a9350a3
Shivam_yogi <shivamyogi99@gmail.com>
2024-01-30 09:55:53 +0530

    more on objects

---
+++ 02_basics/05_objects3.js
@@ -0,0 +1,30 @@
+const course  = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// console.log(course.courseInstructor)
+
+// const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor)
+
+console.log(instructor)
+
+
+// API
+
+// {
+//     'name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+
+[
+    {},
+    {},
+    {}
+]
