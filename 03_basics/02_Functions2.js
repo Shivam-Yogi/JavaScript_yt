b1051907e0e69eab05deca0d356859fde15955d4
Shivam_yogi <shivamyogi99@gmail.com>
2024-01-30 10:31:33 +0530

    more on functions

---
+++ 03_basics/02_Functions2.js
@@ -0,0 +1,50 @@
+// function calculateCartPrice(...num1) {
+//     return num1;
+// }
+// function calculateCartPrice(val1, val2, ...num1) {
+//     return num1;
+// }
+
+// // console.log(calculateCartPrice(2));
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+
+const user = {
+    username: "Shivam",
+    age: 21
+}
+
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and age is ${anyobject.age}`)
+}
+
+
+// handleObject(user)
+
+handleObject({
+    username: "Sam",
+    age: 39
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray) {
+    return  getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200, 400, 500, 1000]));
+
+
+
+
+
+
+
+
+
+
